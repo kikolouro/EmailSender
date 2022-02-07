15d007fb9dcfbfba22a34013c854c2e64fccfd61
Francisco Louro <francisco.louro@infosistema.com>
2022-02-07 18:50:32 +0000

    Discovery rule applied

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python:3.10.0
+COPY mailsenderpy-0.0.1-py3-none-any.whl /mailsenderpy-0.0.1-py3-none-any.whl
+RUN pip3 install /mailsenderpy-0.0.1-py3-none-any.whl
 
 COPY requirements.txt /requirements.txt
 RUN pip3 install -r /requirements.txt
--- sendemail.py
+++ sendemail.py
@@ -7,27 +7,21 @@ import sys
 import pprint
 import logging
 pp = pprint.PrettyPrinter(indent=4)
+from mailsenderpy import MailSenderWAPIClient
+from mailsenderpy.models import EmailContent, Attachment
 
 
-def sendEmail(receivers, senderdata, data, port=465, smtpserver='smtp.gmail.com'):
-    sender_email = senderdata['email']
-    password = senderdata['password']
-    SUBJECT = f"Error on {data['name']} Log"
-    msg = MIMEMultipart('alternative')
-    msg['From'] = sender_email
-    msg['Subject'] = SUBJECT
-    TEXT = f"Error on log: {data['bodydata']['name']}\nLog Value: {data['bodydata']['value']}\nHost: {data['bodydata']['host']}"
-    context = ssl.create_default_context()
+def sendEmail(receivers, senderdata, data):
     try:
-        with smtplib.SMTP_SSL(smtpserver, port, context=context) as server:
-            server.login(sender_email, password)
-            for receiver in receivers:
-                msg['To'] = ''.join(receiver)
-                message = 'To: {}\nSubject: {}\n\n{}'.format(
-                    receiver, SUBJECT, TEXT)
-                server.sendmail(sender_email, receiver, message.encode('utf-8'))
-                logging.info(
-                    f"sent email for the logid: {data['bodydata']['logid']} to {receiver}")
-        return "Success"
+        msenderclient = MailSenderWAPIClient(
+    uri="https://msenderapi_qual.docdigitizer.com", 
+    appKey=senderdata['SENDER_APPKEY'], 
+    privateKey=senderdata['SENDER_KEYPATH'])
+
+        msenderclient.postMessage(EmailContent(
+        toAddresses=receivers,
+        subject=f"Error on {data['name']} Log",
+        body=f"Error on log: {data['bodydata']['name']}\nLog Value: {data['bodydata']['value']}\nHost: {data['bodydata']['host']}"
+    ), schedule=None)
     except Exception as e:
         logging.error("ErrorType : {}, Error : {}".format(type(e).__name__, e))
